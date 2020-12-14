361063d80db078662ece129b20ae86de3fb58590
Evan Mulawski <evan_mulawski@uml.edu>
2020-12-14 13:01:17 -0500

    initial commit

---
+++ XFIssues/XFIssues/CalendarAllDayEventsBadLayoutOnRotate/ViewModels/CalendarViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Telerik.XamarinForms.Input;
+
+namespace XFIssues.CalendarAllDayEventsBadLayoutOnRotate.ViewModels
+{
+    public sealed class CalendarViewModel
+    {
+        public CalendarViewModel()
+        {
+            var now = DateTime.Now;
+
+            Appointments = new List<Appointment>
+            {
+                new Appointment
+                {
+                    Title = "Event 1",
+                    IsAllDay = true,
+                    StartDate = now.Date,
+                    EndDate = now.Date.AddDays(1).AddMilliseconds(-1)
+                }
+            };
+        }
+
+        public List<Appointment> Appointments { get; }
+    }
+}
---
+++ XFIssues/XFIssues/CalendarAllDayEventsBadLayoutOnRotate/Views/CalendarPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+using XFIssues.CalendarAllDayEventsBadLayoutOnRotate.ViewModels;
+
+namespace XFIssues.CalendarAllDayEventsBadLayoutOnRotate.Views
+{
+    public partial class CalendarPage : ContentPage, IXFIssueMarker
+    {
+        CalendarViewModel _viewModel;
+
+        public CalendarPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new CalendarViewModel();
+
+            Calendar.DisplayDate = DateTime.Today;
+        }
+    }
+}
